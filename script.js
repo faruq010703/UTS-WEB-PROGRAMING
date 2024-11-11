e984301cf1d004e9beba73430c9b0dcfbc265d5c
Faruq <faruqmodar@gmail.com>
2024-11-11 07:02:11 -0800

    fromdata objek

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 document.getElementById("loginform").addEventListener("submit", function (event) {
     event.preventDefault();
+    const formData = new FormData();
+    formData.append("username", username);
+    formData.append("password", password);
