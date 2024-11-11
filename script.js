4d28acd51e24151ef8ae6d8cef7cf03c8bd2d9ee
Faruq <faruqmodar@gmail.com>
2024-11-11 07:37:41 -0800

    username

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.getElementById("loginform").addEventListener("submit", function (event) {
     event.preventDefault();
     const formData = new FormData();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    
     formData.append("username", username);
     formData.append("password", password);
